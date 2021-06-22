18e663367d842b595b9d2d4bc0cf61ca945d0d0c
ARULPRIYAM30 <77484841+ARULPRIYAM30@users.noreply.github.com>
2021-06-22 12:33:12 +0530

    Update executeDataStreams.bat

--- executeDataStreams.bat
+++ executeDataStreams.bat
@@ -1,6 +1,6 @@
 pushd "%~dp0"
 
-dotnet run --project Sender\Sender\Sender.csproj   | java -jar ./src/Receiver.jar 
+dotnet run --project Sender\Sender\Sender.csproj | java -jar ./src/Receiver.jar 
 
 popd
 
