30919b1f91cf57ecd8ecbc16e5c796305a8682fb
Rizwan Nargundkar <UmarShaikMNargundkar.Rizwan@in.bosch.com>
2021-06-22 13:38:43 +0530

    Limit in rows to run in git

--- Sender/Sender/Program.cs
+++ Sender/Sender/Program.cs
@@ -12,12 +12,17 @@ namespace Sender
         private static string _filePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\", @"BatteryDataStream/Data/Parameters.csv"));
         private static int _counter = 1;
         private static CSVParameterSource _csvParameterSource;
+        private static int _limitRows = 0;
 
         static void Main(string[] args)
         {
             _csvParameterSource = new CSVParameterSource();
             _batteryDataStream = new DataStreaming(_csvParameterSource);
             _batteryDataStream.Load(_filePath);
+            if (args.Length > 0 && int.TryParse(args[0], out int result))
+            {
+                _limitRows = result;
+            }
 
             Console.WriteLine("Streaming is started in csv format(stateofcharge,temperature). Press Ctrl+C to end");
             Console.CancelKeyPress += (sender, eventArgs) =>
@@ -34,12 +39,19 @@ namespace Sender
         {
             while (!cancellationToken.IsCancellationRequested)
             {
-                if (_counter == _csvParameterSource.MaxRows)
-                    _counter = 1;
+                ResetCounter();
                 Console.WriteLine(_batteryDataStream.Start(_counter));
-                Thread.Sleep(1000);
+                Thread.Sleep(500);
+                if (_limitRows == _counter)
+                    break;
                 _counter++;
             }
+        }
+
+        private static void ResetCounter()
+        {
+            if (_counter == _csvParameterSource.MaxRows)
+                _counter = 1;
         }
     }
 }
